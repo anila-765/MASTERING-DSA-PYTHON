fa647e5e5b601197195c11aeb9697e4b418baeef
Anila Benny <144672603+anila-765@users.noreply.github.com>
2026-06-07 12:49:36 +0530

    Add program to print integer, string, and char inputs

---
+++ week1/problemset1.py
@@ -0,0 +1,20 @@
+'''
+Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
+
+
+Input:  2 Name y
+
+Expected Output:
+
+2
+
+Name
+
+y
+'''
+n1=int(input())
+n2=input()
+n3=input()
+print(n1)
+print(n2)
+print(n3)
