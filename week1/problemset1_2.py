5ae64290942b6a8a67dcfb545728c324ff0a9185
Anila Benny <144672603+anila-765@users.noreply.github.com>
2026-06-07 12:54:46 +0530

    Correct triangle formation logic and output message
    Fix typo in output message and correct angle sum calculation.

--- week1/problemset1_2.py
+++ week1/problemset1_2.py
@@ -11,3 +11,11 @@ Triangle cannot be formed
 
 Explanation -> We are getting 3 inputs, that is three angles of triangle, but here the sum of three angles that is 45+45+45 is not equal to 180 so Triangle cannot be formed is the output.
 '''
+n1=int(input())
+n2=int(input())
+n3=int(input())
+s=n1+n2+n3
+if s==180:
+    print("Triangle ca be formed")
+else:
+    print("triangle  cannot be formed")
