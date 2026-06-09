83b2192f3489af58de961cef8c5a28aa2eb2f20c
Anila Benny <144672603+anila-765@users.noreply.github.com>
2026-06-09 17:57:33 +0530

    Add pattern printing script in pattern14.py
    This script prints a pattern of numbers based on the input value n.

---
+++ week2/pattern14.py
@@ -0,0 +1,32 @@
+
+'''
+Input:
+
+n= 6
+
+1 
+
+2 2 
+
+3 3 3 
+
+4 4 4 4 
+
+5 5 5 5 5 
+
+6 6 6 6 6 6 
+'''
+
+
+
+n=int(input())
+
+for i in range(1,n+1):
+    c=i
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
+
+
+        
+        
